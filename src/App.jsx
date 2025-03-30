6a4d14195c236928335c0fc2a470389f33688d4c
prathameshvasekar <prathamesh.vasekar@bajajfinserv.in>
2025-03-30 16:15:06 +0530

    update prop.js

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import State from './State';
 import StateClass from './State';
 import Props from './Props';
 import { useState } from 'react';
-import { PropsClassComp } from './Props'; 
+import { PropsClassComp } from './Props';
 import InputField from './InputField'
 import Form from './Form'
 import IfElseCondn from './IfElseCondn'
@@ -15,64 +15,64 @@ import FormValidations from './FormValidations'
 
 
 
-
-
 function App() {
 
-  
-function AppleInside(){
-  return (
-    <div>
-      AppleInside Functn..
-    </div>
-  )
-}
 
-function appFunctn(){
-  alert('In app function');
-}
-// Props data
-const data1 = {name:'prathamesh', email:'prathamesh@gmail.com'}
-const [data2, setData] = useState({name:'prathamesh', email:'prathamesh@gmail.com'})
+  function AppleInside() {
+    return (
+      <div>
+        AppleInside Functn..
+      </div>
+    )
+  }
+
+  function appFunctn() {
+    alert('In app function');
+  }
+
+  // Props data
+  const data1 = { name: 'prathamesh', email: 'prathamesh@gmail.com' }
+  const [data2, setData] = useState({ name: 'prathamesh', email: 'prathamesh@gmail.com' })
   return (
     <div className="App">
-      {/* 
+
       <AppleInside />
       <AppleOutside />
       <User />
       <Classcomponent></Classcomponent>
 
-      <ClickEvent /> 
-      
-      <State /> 
+      <ClickEvent />
+
+      <State />
       <StateClass />
 
-      <Props params= {data1} /> 
-      <Props params= {data2} />
-     
-      <Props params= {AppleInside} />
-      <button onClick={()=>{
-        setData({name:'Vasekar', email:'vasekar@gmail.com'})
+      <Props params={data1} />
+      <Props params={data2} />
+
+      <Props params={AppleInside} />
+      <button onClick={() => {
+        setData({ name: 'Vasekar', email: 'vasekar@gmail.com' })
       }}>update data2</button>
-      <PropsClassComp name='hiiiii' ></PropsClassComp> 
+      <PropsClassComp name='hiiiii' ></PropsClassComp>
 
       <InputField></InputField>
-      
+
       <Form />
       <IfElseCondn />
 
+      {/* 
       */}
-      <Props params= {appFunctn} />
+      <Props params={appFunctn} />
 
       <FormValidations />
     </div>
   );
 }
 
-function AppleOutside(){
+function AppleOutside() {
   return (
     <div>
-      AppleOutside Functn.. {10+20}
+      AppleOutside Functn.. {10 + 20}
     </div>
   )
 }
--- src/Props.js
+++ src/Props.js
@@ -1,26 +1,26 @@
 import React from 'react';
 
-function Props(props){
-    console.log('props - ',props)
+function Props(props) {
+    console.log('props - ', props)
     const params = props.params
     return (
         <div>
             <h2>student name = {params.name}</h2>
             <h2>student email = {params.email}</h2>
-            <button onClick={()=>params()}>call app function </button>
+            <button onClick={() => params()}>call app function </button>
         </div>
     )
 }
 
 export default Props;
 
-// export class PropsClassComp extends React.Component{
+export class PropsClassComp extends React.Component {
 
-//     render(){
-//         return (
-//             <div>
-//                 <h2>student name = {this.props.name}</h2>
-//             </div>
-//         )
-//     }
-// }
+    render() {
+        return (
+            <div>
+                <h2>student name = {this.props.name}</h2>
+            </div>
+        )
+    }
+}
